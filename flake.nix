e3eb2f6963d64cbe2f5ff3547c8c6fba42b0dfa4
Franco Biasin <franconbiasin+github@gmail.com>
2023-09-19 12:46:25 -0300

    Remove android.nix, leave everything in flake.nix

--- flake.nix
+++ flake.nix
@@ -15,15 +15,31 @@
           config.allowUnfree = true;
           config.android_sdk.accept_license = true;
         };
-      in {
+      in
+      {
         devShells.default =
-          let android = pkgs.callPackage ./nix/android.nix { };
-          in pkgs.mkShell {
-            buildInputs = with pkgs; [
-              # from pkgs
-              flutter
-              jdk11
-              #from ./nix/*
+          let android = pkgs.androidenv.composeAndroidPackages {
+            toolsVersion = "26.1.1";
+            platformToolsVersion = "33.0.3";
+            buildToolsVersions = [ "30.0.3" ];
+            includeEmulator = false;
+            emulatorVersion = "30.3.4";
+            platformVersions = [ "28" "29" "30" "31" ];
+            includeSources = false;
+            includeSystemImages = false;
+            systemImageTypes = [ "google_apis_playstore" ];
+            abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+            cmakeVersions = [ "3.10.2" ];
+            includeNDK = true;
+            ndkVersions = [ "22.0.7026061" ];
+            useGoogleAPIs = false;
+            useGoogleTVAddOns = false;
+          };
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.flutter
+              pkgs.jdk11
               android.androidsdk # adds `emulator` to path
               android.platform-tools
             ];
